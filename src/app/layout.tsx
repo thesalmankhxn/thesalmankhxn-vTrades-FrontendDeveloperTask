53bb38fe54e58baa900eb85026bb11dd29202e90
thesalmankhxn <thesalmankhxn@gmail.com>
2025-06-27 16:22:57 +0530

    add: source sans pro font;

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,18 @@
 import type { ReactNode } from 'react';
 
 import type { Metadata } from 'next';
-import localFont from 'next/font/local';
+import { Source_Sans_3 } from 'next/font/google';
 
 import { ThemeProvider } from 'next-themes';
 
 import '@/app/globals.css';
 
-const geistSans = localFont({
-    src: './fonts/GeistVF.woff',
-    variable: '--font-geist-sans',
-    weight: '100 900'
-});
-const geistMono = localFont({
-    src: './fonts/GeistMonoVF.woff',
-    variable: '--font-geist-mono',
-    weight: '100 900'
+// Import Source Sans Pro from Google Fonts
+const sourceSansPro = Source_Sans_3({
+    subsets: ['latin'],
+    variable: '--font-source-sans-pro',
+    weight: ['200', '300', '400', '500', '600', '700', '900'],
+    display: 'swap'
 });
 
 export const metadata: Metadata = {
@@ -28,7 +25,7 @@ const Layout = ({ children }: Readonly<{ children: ReactNode }>) => {
         // ? https://github.com/pacocoursey/next-themes?tab=readme-ov-file#with-app
         // ? https://react.dev/reference/react-dom/client/hydrateRoot#suppressing-unavoidable-hydration-mismatch-errors
         <html suppressHydrationWarning lang='en'>
-            <body className={`${geistSans.variable} ${geistMono.variable} bg-background text-foreground antialiased`}>
+            <body className={`${sourceSansPro.variable} bg-background text-foreground antialiased`}>
                 <ThemeProvider attribute='class'>{children}</ThemeProvider>
             </body>
         </html>
