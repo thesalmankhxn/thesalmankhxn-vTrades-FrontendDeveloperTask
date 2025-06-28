331b5dd1c0f187983e3cc5ca3ef380ba946d2330
thesalmankhxn <thesalmankhxn@gmail.com>
2025-06-28 15:28:47 +0530

    fix: build;

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,71 +1,33 @@
-import pluginJs from '@eslint/js';
-import nextPlugin from '@next/eslint-plugin-next';
-
-import eslintConfigPrettier from 'eslint-config-prettier';
-import importPlugin from 'eslint-plugin-import';
-import pluginPromise from 'eslint-plugin-promise';
-import pluginReact from 'eslint-plugin-react';
-import tailwind from 'eslint-plugin-tailwindcss';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
 
 export default [
-    {
-        files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}']
+  { ignores: ['dist'] },
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
     },
-    {
-        languageOptions: {
-            ecmaVersion: 'latest',
-            globals: { ...globals.browser, ...globals.node }
-        }
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
     },
-    {
-        settings: {
-            react: {
-                version: 'detect'
-            }
-        }
+    rules: {
+      ...js.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
     },
-    pluginJs.configs.recommended, // ? https://github.com/eslint/eslint
-    importPlugin.flatConfigs.recommended, // ? https://github.com/import-js/eslint-plugin-import
-    ...tseslint.configs.recommended, // ? https://github.com/typescript-eslint/typescript-eslint
-    pluginPromise.configs['flat/recommended'], // ? https://github.com/eslint-community/eslint-plugin-promise
-    pluginReact.configs.flat.recommended, // ? https://github.com/jsx-eslint/eslint-plugin-react
-    pluginReact.configs.flat['jsx-runtime'], // ? https://github.com/jsx-eslint/eslint-plugin-react
-    eslintConfigPrettier, // ? https://github.com/prettier/eslint-config-prettier
-    ...tailwind.configs['flat/recommended'], // ? https://github.com/francoismassart/eslint-plugin-tailwindcss
-    {
-        rules: {
-            'no-unused-vars': 'off',
-            'react/react-in-jsx-scope': 'off',
-            'react-hooks/exhaustive-deps': 'off',
-            'react/display-name': 'off',
-            'react/prop-types': 'off',
-            'newline-before-return': 'error',
-            '@typescript-eslint/no-unused-vars': 'off',
-            '@typescript-eslint/no-unused-expressions': 'off',
-            'tailwindcss/no-custom-classname': 'off',
-            'tailwindcss/migration-from-tailwind-2': 'off',
-            'import/no-unresolved': 'off',
-            'import/no-named-as-default': 'off'
-        }
-    },
-    // ! ===================== DISCLAIMER =====================
-    // ! There is no official solution available for new ESLint 9 flat config structure for NextJS
-    // ! The solution is taken from the community and may not be the best practice, use it at your own risk
-    // ? Ref: https://github.com/vercel/next.js/discussions/49337?sort=top#discussioncomment-5998603
-    // ! ======================================================
-    {
-        plugins: {
-            '@next/next': nextPlugin
-        },
-        rules: {
-            ...nextPlugin.configs.recommended.rules,
-            ...nextPlugin.configs['core-web-vitals'].rules,
-            '@next/next/no-img-element': 'off'
-        }
-    },
-    {
-        ignores: ['.next/*']
-    }
-];
+  },
+]
